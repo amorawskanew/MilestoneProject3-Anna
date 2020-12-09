e0f4758d65412768ecd4925ad19e154e5f643c34
amorawskanew <amorawska@yahoo.com>
2020-12-09 00:53:10 +0000

    add

--- app.py
+++ app.py
@@ -35,79 +35,15 @@ mongo = PyMongo(app)
 @app.route('/')
 def home():
     return render_template('home.html')
-    
+
 
  # Routes to  "coctails by categories"   
 @app.route('/get_coctail_by_category/<category>')
 def get_coctail_by_category(category):
     return render_template('coctails_by_category.html',
      coctails=mongo.db.coctails.find({"category_name":category}).sort("coctail_name"))     
-
-
-# Routes to category "Classics"
-@app.route('/get_classics')
-def get_classics():
-    return render_template('classics.html', 
-                           # sort coctails in categories by coctail name
-                           coctails=mongo.db.coctails.find({"category_name":"Classics"}).sort("coctail_name"))
-    
-    
-# Routes to category "New era drinks"
-@app.route('/get_new_era_drinks')
-def get_new_era_drinks():
-    return render_template('new_era_drinks.html', 
-                           # sort coctails in categories by coctail name
-                           coctails=mongo.db.coctails.find({"category_name":"New era drinks"}).sort("coctail_name"))
-
-
-# Routes coctails to category "Specials"
-@app.route('/get_specials')
-def get_specials():
-    return render_template('specials.html', 
-                           # sort coctails in categories by coctail name
-                           coctails=mongo.db.coctails.find({"category_name":"Specials"}).sort("coctail_name"))
-
-
-# Routes coctails to category "Flaming coctails"
-@app.route('/get_flaming_coctails')
-def get_flaming_coctails():
-    return render_template('flaming_coctails.html', 
-                           # sort coctails in categories by coctail name
-                           coctails=mongo.db.coctails.find({"category_name":" Flaming coctails"}).sort("coctail_name"))
-
-
-# Routes coctails to category "Beer coctails"
-@app.route('/get_beer_coctails')
-def get_beer_coctails():
-    return render_template('beer_coctails.html', 
-                           # sort coctails in categories by coctail name
-                           coctails=mongo.db.coctails.find({"category_name":" Beer coctails"}).sort("coctail_name"))
-
- 
- 
-# Routes coctails to category "Liquor coctails"
-@app.route('/get_liquor_coctails')
-def get_liquor_coctails():
-    return render_template('liquor_coctails.html', 
-                           # sort coctails in categories by coctail name
-                           coctails=mongo.db.coctails.find({"category_name":"Liquor coctails"}).sort("coctail_name")) 
-
-# Routes coctails to category "Wine coctails"
-@app.route('/get_wine_coctails')
-def get_wine_coctails():
-    return render_template('wine_coctails.html', 
-                           # sort coctails in categories by coctail name
-                           coctails=mongo.db.coctails.find({"category_name":"Wine coctails"}).sort("coctail_name")) 
-
-
-# Routes coctails to category "Wine coctails"
-@app.route('/get_non_alcoholic_coctails')
-def get_non_alcoholic_coctails():
-    return render_template('non_alcoholic_coctails.html', 
-                           # sort coctails in categories by coctail name
-                           coctails=mongo.db.coctails.find({"category_name":"Non-alcoholic coctails"}).sort("coctail_name")) 
-           
-              
+       
+       
 # routes page to all coctails in my DB   
 @app.route('/get_coctail')
 def get_coctail():
